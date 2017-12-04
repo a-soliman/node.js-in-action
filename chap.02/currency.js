7bf4714e2fdbc9885e48f89cc44862ee503b619b
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-04 08:01:41 -0800

    converted the currency to a ES6 class exported via module.exports instead of exports

--- chap.02/currency.js
+++ chap.02/currency.js
@@ -1,8 +1,47 @@
-const canadianDollar = 0.91;
+class Currency {
+	constructor(canadianDollar) {
+		this.canadianDollar = canadianDollar || 0.91;
+	}
 
-function roundTwo (amount) {
-	return Math.round(amount * 100) / 100;
-};
+	roundTwoDecimals (amount) {
+		return Math.round(amount * 100) / 100;
+	};
 
-exports.candadianToUs = canadian => roundTwo(canadian * canadianDollar);
-exports.usToCanadian  = us => roundTwo(us / canadianDollar);
+	canadianToUs(canadian) {
+		return this.roundTwoDecimals(canadian * this.canadianDollar);
+	}
+
+	usToCanadian(us) {
+		return this.roundTwoDecimals(us / this.canadianDollar);
+	};
+}
+
+module.exports = Currency;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const canadianDollar = 0.91;
+
+// function roundTwo (amount) {
+// 	return Math.round(amount * 100) / 100;
+// };
+
+// exports.candadianToUs = canadian => roundTwo(canadian * canadianDollar);
+// exports.usToCanadian  = us => roundTwo(us / canadianDollar);
--- chap.02/test-currency.js
+++ chap.02/test-currency.js
@@ -1,7 +1,10 @@
-const currency = require('./currency');
+const Currency = require('./currency');
 
-console.log(' 50 Canadian dollars equal this amount of US dollars.')
-console.log(currency.candadianToUs(50));
 
-console.log(' 30 US dollars equal this amount of Canadian dollars');
-console.log(currency.usToCanadian(30));
+// console.log(' 50 Canadian dollars equal this amount of US dollars.')
+// console.log(currency.canadianToUs(50));
+
+// console.log(' 30 US dollars equal this amount of Canadian dollars');
+// console.log(currency.usToCanadian(30));
+
+console.dir(Currency)
