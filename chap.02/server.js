a3fb9e06614e251d4ef79e8f5118ecad3c0d13bc
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-04 08:23:53 -0800

    coded the server file to parse the json list file as well as the index.html file to responed to a server call with replaceing the % char in the html with the list of json strings

--- chap.02/server.js
+++ chap.02/server.js
@@ -1,13 +1,27 @@
 const http = require('http');
+const fs 	= require('fs');
 
 
-const server = http.createServer();
-
-function handleRequest() {
-	console.log('handled request')
-}
-server.on('request', handleRequest);
-
-server.listen(3000, () => {
-	console.log('server is on at 3000;')
-})
+http.createServer(( req, res ) => {
+	if( req.url == '/') {
+		fs.readFile('./list.json', ( err, data ) => {
+			if( err ) {
+				console.error(err);
+				res.end('Server Error');
+			} else {
+				const titles = JSON.parse(data.toString());
+				fs.readFile('./index.html', ( err, data ) => {
+					if( err ) {
+						console.error(err);
+						res.end('Server Error');
+					} else {
+						const templ = data.toString();
+						const html = templ.replace('%', titles.join('</li><li>'));
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.end(html)
+					}
+				})
+			}
+		})
+	}
+}).listen(3000)
