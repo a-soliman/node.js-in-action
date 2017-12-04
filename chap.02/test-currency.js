f7827616453dc788d89096d6bab9e606a8b69f03
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-04 07:30:12 -0800

    required the module and tested it's functions

---
+++ chap.02/test-currency.js
@@ -0,0 +1,7 @@
+const currency = require('./currency');
+
+console.log(' 50 Canadian dollars equal this amount of US dollars.')
+console.log(currency.candadianToUs(50));
+
+console.log(' 30 US dollars equal this amount of Canadian dollars');
+console.log(currency.usToCanadian(30));
