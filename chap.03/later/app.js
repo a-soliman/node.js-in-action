21f03332c230214c1f12969b58391f6fd20aa527
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-05 08:21:36 -0800

    coded an out line for the RESTful API routes

--- chap.03/later/app.js
+++ chap.03/later/app.js
@@ -1,12 +1,39 @@
 const express		= require('express');
 
 const app 	= express();
-const port 	= process.env.PORT || 3000;
+const articles = [{ title: 'Exmaple' }];
 
-app.get ('/', ( req, res ) => {
-	res.send('Hello world!');
+app.set('port', process.env.PORT || 3000);
+
+
+// GET ARTICLES
+app.get ('/articles', ( req, res, next) => {
+	res.send(articles);
 });
 
-app.listen(port, () => {
-	console.log(`Express web app is avialable at ${port}`);
+// GET ARTICLE
+app.get('/articles/:id', ( req, res, next ) => {
+	let id = req.param.id;
+	console.log('fitching: ', id);
+	res.send(articles[id]);
+});
+
+// POST ARTICLE
+app.post('/articles', ( req, res ) => {
+	res.send('Save an article');
+});
+
+// DELETE ARTICLE
+app.delete('/articles/:id', ( req, res ) => {
+	let id = req.param.id;
+	console.log('Deleting: ', id);
+	delete articles[id];
+	res.send({ message: 'Deleted' });
+});
+
+
+app.listen(app.get('port'), () => {
+	console.log(`Express web app is avialable at ` + app.get('port'));
 })
+
+module.exports = app;
